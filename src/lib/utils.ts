b89a3de99fcdfec5dfdcbe7f74a5cce2a61bc8a8
Hengrui Xing <henryxing@Hengruis-MacBook-Pro.local>
2025-10-27 00:59:13 -0400

    schema update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function nullthrows<T>(
+  value: T | null | undefined,
+  message?: string,
+): NonNullable<T> {
+  if (value != null) {
+    return value;
+  }
+  throw new TypeError(
+    message ?? `Expected value not to be null or undefined but got ${value}`,
+  );
 }
